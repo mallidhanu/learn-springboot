48cb7982ad1898653f124083ad167f7c431880e1
mallidhanu <mallidhanu550@gmail.com>
2026-05-17 12:48:04 +0530

    comment line removed

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -100,7 +100,7 @@ public class EmployeeController {
         Employee employee = employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id: " + id));
 
         employeeRepository.delete(employee);
-        System.out.println("deleted succeessfully "+ employee);
+        System.out.println("deleted "+ employee);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
